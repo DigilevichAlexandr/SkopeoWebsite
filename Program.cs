0f3292daf2a60b05cd8015d93bca9fa858dd4c8f
alexandr digilevich <digilevich.alexandr@gmail.com>
2025-08-24 15:17:34 +0300

    fix

--- Program.cs
+++ Program.cs
@@ -4,8 +4,6 @@ using SkopeoWebsite;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
